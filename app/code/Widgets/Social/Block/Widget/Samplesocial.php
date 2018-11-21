10a115edeec3f40e384fd69aa61b9d5c2b3a867d
Guilherme Engler <g.eengler@gmail.com>
2018-11-21 00:56:09 -0200

    Widget social

---
+++ app/code/Widgets/Social/Block/Widget/Samplesocial.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Widgets\Social\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Samplesocial extends Template implements BlockInterface
+{
+
+    protected $_template = "widget/samplesocial.phtml";
+
+}
